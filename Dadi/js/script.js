0d68b4f8a15cae0c9458babce9266f59dbac5772
comemaino <mat.maino@gmail.com>
2022-04-12 16:20:31 +0200

    Add: /Dadi *

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -1,4 +1,27 @@
-// GENERARE PC RANDOM NUMBER
-// GENERARE USER RANDOM NUMBER
-// COMPARARE I NUMERI
-//STAMBARE NUMERI E OUTCOME
+// CHIAMARE BOTTONE
+const playBtn = document.getElementById("play-btn");
+// ASCOLTARE BOTTONE
+playBtn.addEventListener("click", function () {
+	// GENERARE PC RANDOM NUMBER
+	const pcNumber = Math.floor(Math.random() * (7 - 1) + 1);
+	console.log(pcNumber);
+	// COMPILARE PC NUMBER
+	document.getElementById("pc-number").innerHTML = pcNumber;
+	// GENERARE USER RANDOM NUMBER
+	const userNumber = Math.floor(Math.random() * (7 - 1) + 1);
+	// COMPILARE USER NUMBER
+	document.getElementById("user-number").innerHTML = userNumber;
+	console.log(userNumber);
+	// COMPARARE I NUMERI
+	let outcome;
+	if (userNumber > pcNumber) {
+		outcome = "Hai vinto!";
+	} else if (userNumber < pcNumber) {
+		outcome = "Hai perso, ritenta!";
+	} else {
+		outcome = "Pareggio!";
+	}
+	console.log(outcome);
+	//STAMBARE NUMERI E OUTCOME
+	document.getElementById("outcome").innerHTML = outcome;
+});
